4badc7ab71d3e913ef35430663067705fc850692
Toby Ye <toby9186@outlook.com>
2023-11-03 18:15:08 +0800

    🍑 修改仪表盘页

---
+++ src/pages/dashboard/base/components/Announcement.vue
@@ -0,0 +1,11 @@
+<template>
+  <t-card title="公告" class="row-container">
+    <p>欢迎使用 Intelligent Audit Tools！</p>
+  </t-card>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+</script>
+
+<style lang="less" scoped></style>
--- src/pages/dashboard/base/index.vue
+++ src/pages/dashboard/base/index.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
-    <!-- 顶部 card  -->
+    <announcement />
     <top-panel class="row-container" />
-    <!-- 中部图表  -->
     <middle-chart class="row-container" />
-    <!-- 列表排名 -->
-    <rank-list class="row-container" />
-    <!-- 出入库概览 -->
-    <output-overview class="row-container" />
+    <!-- <rank-list class="row-container" /> -->
+    <!-- <output-overview class="row-container" /> -->
   </div>
 </template>
 
@@ -18,6 +15,7 @@ export default {
 </script>
 
 <script setup lang="ts">
+import Announcement from './components/Announcement.vue';
 import MiddleChart from './components/MiddleChart.vue';
 import OutputOverview from './components/OutputOverview.vue';
 import RankList from './components/RankList.vue';
