a8dd457c8e9cb82155e48e1d0100d285b3a4239f
Toby Ye <508087823@qq.com>
2023-11-23 16:04:19 +0800

    🍑 http请求最新版本

--- src/pages/dashboard/base/components/SoftwareInfo.vue
+++ src/pages/dashboard/base/components/SoftwareInfo.vue
@@ -9,6 +9,7 @@
       :hover="hover"
       :table-layout="tableLayout ? 'auto' : 'fixed'"
       :show-header="showHeader"
+      :loading="loading"
       cell-empty-content="-"
       resizable
       lazy-load
@@ -19,11 +20,13 @@
 
 <script lang="ts" setup>
 import { getTauriVersion, getVersion } from '@tauri-apps/api/app';
+import { getClient } from '@tauri-apps/api/http';
 import axios from 'axios';
 import { ref } from 'vue';
 
 import { request } from '@/utils/request';
 
+const loading = ref(true);
 const stripe = ref(true);
 const bordered = ref(true);
 const hover = ref(false);
@@ -39,10 +42,32 @@ const getSysInfo = async () => {
   const appVersion = await getVersion();
   const tauriVersion = await getTauriVersion();
 
-  data.value.push({ name: 'Intelligent Audit Tools', value: `Ver. ${appVersion} ` });
-  data.value.push({ name: 'Tauri', value: tauriVersion });
+  const client = await getClient();
+  const response = await client.request<Updater>({
+    method: 'GET',
+    url: 'https://github.com/CupOfBread/Intelligent-Audit-Tools/releases/download/updater/update.json',
+  });
+
+  const latestVersion = response.data.version;
+  if (latestVersion !== appVersion) {
+    data.value.push({
+      name: 'Intelligent Audit Tools',
+      value: `Ver. ${appVersion}      (发现新版本 Ver. ${latestVersion})`,
+    });
+  } else {
+    data.value.push({ name: 'Intelligent Audit Tools', value: `Ver. ${appVersion}` });
+  }
+
+  data.value.push({ name: 'Tauri', value: `Ver. ${tauriVersion}` });
+  loading.value = false;
 };
 getSysInfo();
+
+interface Updater {
+  version: string;
+  notes: string;
+  pub_date: Date;
+}
 </script>
 
 <style lang="less" scoped></style>
